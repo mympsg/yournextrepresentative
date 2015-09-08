c1a3992a337076272f6b685a5be4bb41623fe7a1
Chow Loong Jin <hyperair@debian.org>
2015-09-08 12:07:27 +0800

    SG: Set ELECTIONS data for 2015 GE

--- elections/sg_general_election_2015/settings.py
+++ elections/sg_general_election_2015/settings.py
@@ -1,28 +1,12 @@
 from datetime import date
 
 ELECTIONS = {
-    '2010': {
-        'for_post_role': 'Member of Parliament',
-        'candidate_membership_role': 'Candidate',
-        'winner_membership_role': None,
-        'election_date': date(2010, 5, 6),
-        'candidacy_start_date': date(2005, 5, 6),
-        'organization_id': 'commons',
-        'name': '2010 General Election',
-        'current': False,
-        'use_for_candidate_suggestions': True,
-        'party_membership_start_date': date(2005, 5, 6),
-        'party_membership_end_date': date(2010, 5, 6),
-        'mapit_types': ['WMC'],
-        'mapit_generation': 22,
-        'post_id_format': '{area_id}',
-    },
     '2015': {
         'for_post_role': 'Member of Parliament',
         'candidate_membership_role': 'Candidate',
         'winner_membership_role': None,
-        'election_date': date(2015, 5, 7),
-        'candidacy_start_date': date(2010, 5, 7),
+        'election_date': date(2015, 9, 11),
+        'candidacy_start_date': date(2015, 9, 1),
         'organization_id': 'commons',
         'name': '2015 General Election',
         'current': True,
